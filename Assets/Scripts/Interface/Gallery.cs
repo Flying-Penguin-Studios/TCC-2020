c6723188b85b4ef0f0f5132fb7b9ddc8591c5bcb
Gabriel Milani <gabriimilani@gmail.com>
2020-09-03 22:24:08 -0300

    Teste milani

--- Assets/Scripts/Interface/Gallery.cs
+++ Assets/Scripts/Interface/Gallery.cs
@@ -7,7 +7,7 @@ public class Gallery : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Teste
     }
 
     // Update is called once per frame

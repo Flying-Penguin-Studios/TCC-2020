e76d441a159dd99c760d1682e11c3b7cfaab5fac
Unknown <1140368@sga.pucminas.br>
2020-10-31 18:01:31 -0300

    barulho da besta funfa
    aeeeeeeee

---
+++ Assets/Audio/AnimationEvents-Scripts/SwordManSoundEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordManSoundEvents : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] SwordClips;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void SwordManSword()
+    {
+        PlayRandomClip(SwordClips);
+    }
+
+    void PlayRandomClip(AudioClip[] Clips)
+    {
+        audioSource.PlayOneShot(Clips[Random.Range(0, Clips.Length)]);
+    }
+}

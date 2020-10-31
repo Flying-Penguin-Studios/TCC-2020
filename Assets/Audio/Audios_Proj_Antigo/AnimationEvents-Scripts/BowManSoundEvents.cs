60931af9940c8cdb9ee707c91bbe5d78498ef792
Unknown <1140368@sga.pucminas.br>
2020-10-30 22:37:22 -0300

    PROGRESSO IMPORTANTE AUDIO
    é isso ai

--- Assets/Audio/Audios_Proj_Antigo/AnimationEvents-Scripts/BowManSoundEvents.cs
+++ Assets/Audio/Audios_Proj_Antigo/AnimationEvents-Scripts/BowManSoundEvents.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class BowManSoundEvents : MonoBehaviour
 {
-    
+    [SerializeField]
+    private AudioClip[] BowClips;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void BowManBow()
+    {
+        PlayRandomClip(BowClips);
+    }
+
+    void PlayRandomClip(AudioClip[] Clips)
+    {
+        audioSource.PlayOneShot(Clips[Random.Range(0, Clips.Length)]);
+    }
 }

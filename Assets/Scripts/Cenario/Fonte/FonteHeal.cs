0d06c91e039af47bb3cc02d0a2b41b91a8a03644
Toledo <toribas@gmail.com>
2020-11-19 18:44:35 -0300

    VFX de cura nos lagos

--- Assets/Scripts/Cenario/Fonte/FonteHeal.cs
+++ Assets/Scripts/Cenario/Fonte/FonteHeal.cs
@@ -5,10 +5,49 @@ using UnityEngine;
 public class FonteHeal : MonoBehaviour {
 
 
-
+    public GameObject healVFX;
 
     private float fireRate = 1;
     private float nextFire = 0;
+
+    private bool P1Inside = false;
+    private bool P2Inside = false;
+    private bool vfxExist = false;
+
+    GameObject instantiatedHealVFX;
+
+    ParticleSystem vfx1;
+    ParticleSystem vfx2;
+
+
+
+
+    private void FixedUpdate() {
+        
+        if(P1Inside || P2Inside) {
+
+
+            if(!vfxExist) {
+                instantiatedHealVFX = Instantiate(healVFX, this.transform.position, this.transform.rotation);
+                vfx1 = instantiatedHealVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                vfx2 = instantiatedHealVFX.transform.GetChild(1).GetComponent<ParticleSystem>();
+                vfxExist = true;
+            }            
+
+            instantiatedHealVFX.SetActive(true);
+
+        } else {
+
+            if(instantiatedHealVFX) {
+                vfx1.Stop();
+                vfx2.Stop();
+                Destroy(instantiatedHealVFX, 5);
+                vfxExist = false;
+            }
+            
+        }
+
+    }
 
 
 
@@ -17,14 +56,25 @@ public class FonteHeal : MonoBehaviour {
         
         if(other.CompareTag("Player")) {
 
-            if(Time.time >= nextFire) {
+            if(other.name == "Player1") { P1Inside = true; }
+            if(other.name == "Player2") { P2Inside = true; }
 
+            if(Time.time >= nextFire) {
                 other.GetComponent<PlayerController>().Heal(1);
-                nextFire = Time.time + fireRate;
+                nextFire = Time.time + fireRate;                
             }
         }
-
     }
+
+
+    private void OnTriggerExit(Collider other) {
+
+        if(other.CompareTag("Player")) {
+            if (other.name == "Player1") { P1Inside = false; }
+            if (other.name == "Player2") { P2Inside = false; }
+        }
+    }
+
 
 
 
